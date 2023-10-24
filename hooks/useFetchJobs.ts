07604e25bccca31c7467899f4e428a468707c842
jinhojang6 <jinho5051@gmail.com>
2023-10-24 21:23:30 +0900

    chore: add job fetching example

---
+++ hooks/useFetchJobs.ts
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+
+const boardMappings: { [key: string]: string } = {
+  acidtest: 'testacidinfo',
+  logos: 'logos',
+  status: 'status72',
+  nimbus: 'nimbus',
+  codex: 'codex',
+  nomos: 'nomos',
+  statusnetwork: 'thestatusnetwork',
+  ift: 'instituteoffreetechnologies',
+  vac: 'vac',
+  waku: 'waku',
+}
+
+interface Job {
+  title?: string
+  [key: string]: any // Additional fields you might expect from the job object
+}
+
+interface FetchJobsResponse {
+  jobs: Job[]
+  meta: {
+    total: number
+  }
+}
+
+function useFetchJobs() {
+  const [data, setData] = useState<FetchJobsResponse | null>(null)
+  const [error, setError] = useState<Error | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const fetchJobs = async (selectedBoards: string[], titleFilter?: string) => {
+    setIsLoading(true)
+    try {
+      const resultsPerBoard: { [key: string]: Job[] } = {}
+
+      for (let board of selectedBoards) {
+        if (board === 'all') {
+          const promises = Object.entries(boardMappings).map(
+            ([key, boardName]) =>
+              fetch(
+                `https://boards-api.greenhouse.io/v1/boards/${boardName}/jobs`,
+              ).then((response) => response.json()),
+          )
+          const allResults = await Promise.all(promises)
+          allResults.forEach((result, index) => {
+            const boardKey = Object.keys(boardMappings)[index]
+            resultsPerBoard[boardKey] = result.jobs
+          })
+          break
+        } else {
+          const response = await fetch(
+            `https://boards-api.greenhouse.io/v1/boards/${boardMappings[board]}/jobs`,
+          )
+          const jobData = await response.json()
+          resultsPerBoard[board] = jobData.jobs
+        }
+      }
+
+      let allJobs = Object.values(resultsPerBoard).flat()
+
+      if (titleFilter) {
+        allJobs = allJobs.filter(
+          (job) => job.title && job.title.includes(titleFilter),
+        )
+      }
+      console.log(allJobs)
+      setData({ jobs: allJobs, meta: { total: allJobs.length } })
+    } catch (err: any) {
+      setError(err)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return { data, error, isLoading, fetchJobs }
+}
+
+export default useFetchJobs
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,6 +1,8 @@
 import { SEO } from '@/components/SEO'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
+import { useEffect } from 'react'
+import useFetchJobs from '../../hooks/useFetchJobs'
 import { DefaultLayout } from '../layouts/DefaultLayout'
 
 interface Props {
@@ -8,9 +10,45 @@ interface Props {
 }
 
 const Page = ({ mdxSource }: Props) => {
+  const { data, error, isLoading, fetchJobs } = useFetchJobs()
+
+  useEffect(() => {
+    // Fetching jobs from all job boards and with no title filter on component mount
+    fetchJobs(['logos'], '')
+  }, [])
+
+  if (isLoading) {
+    return <div>Loading jobs...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
+
+  if (!data || data.jobs.length === 0) {
+    return <div>No jobs found</div>
+  }
+
   return (
     <>
       <SEO />
+      <div>
+        <h1>Job Listings</h1>
+        <ul>
+          {data.jobs.map((job: any) => (
+            <li key={job.id}>
+              <a
+                href={job.absolute_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {job.title}
+              </a>
+              <p>{job.location?.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
       <MDXRemote {...mdxSource} />
     </>
   )
