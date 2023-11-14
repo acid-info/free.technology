9da4b616e76651072e578792dc0ee87ef45d2b61
jinhojang6 <jinho5051@gmail.com>
2023-11-14 19:54:47 +0900

    chore: update content

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -39,7 +39,7 @@ export async function getStaticProps() {
       id: '1',
       title: 'Technical',
       description:
-        "Vac is a principle-driven research and development group that provides technical support to each IFT startup. It comprises elite specialists in distributed systems, zero-knowledge proofs, and P2P networking, produces specifications for IFT startups, and undertakes comprehensive protocol testing. Like the IFT and the startups with which it works, a commitment to liberty, censorship resistance, security, privacy, decentralisation, and inclusivity guides Vac's R&D efforts.",
+        'Vac is a principle-driven research and development group that provides technical support to each IFT startup. It comprises elite specialists in distributed systems, zero-knowledge proofs, and P2P networking, produces specifications for IFT startups, and undertakes comprehensive protocol testing. Like the IFT and the startups with which it works, a commitment to liberty, censorship resistance, security, privacy, decentralisation, and inclusivity guides Vac’s R&D efforts.',
       button: {
         text: 'Discover Vac',
         url: 'https://vac.dev/',
@@ -49,25 +49,29 @@ export async function getStaticProps() {
       id: '2',
       title: 'Financial',
       description:
-        'Our startups benefit from IFT capital to fund their research and development efforts. Meanwhile, the IFT nurtures relationships with investors who share our values and commitment to world-changing tech. Besides funding and investment, the IFT handles compensation, bonus payments, and expenses incurred for all contributors working with us via our in-house finance department.',
+        'Our startups benefit from IFT capital to fund their research and development efforts. Meanwhile, the IFT nurtures relationships with investors who share our values and commitment to world-changing tech. Besides funding and investment, the IFT handles payment of fees to contributors, and potential expenses incurred by the contributors working with us via our in-house finance department.',
     },
     {
       id: '3',
       title: 'Legal',
       description:
-        'A specialist and highly experienced legal department led by Dr Agata Ferreira helps our startups navigate the often confusing and ever-changing blockchain legal landscape. Supporting Agata is a team of two with backgrounds in crypto finance and blockchain legal issues. The department ensures our startups remain sound from a legal perspective.',
+        'A specialist and highly experienced legal department led by Dr Agata Ferreira helps our startups navigate the often confusing and ever-changing blockchain legal landscape while providing the legal support to allow them to achieve their project aims. Supporting Agata is a team of two with backgrounds in a wide range of legal areas, including, among others, crypto finance and blockchain. The department assists our startups identify and prevent or mitigate potential (legal) risks.',
     },
     {
       id: '4',
-      title: 'HR',
+      title: 'People Operations',
       description:
-        'Our PeopleOps department enables contributors across the IFT to focus on developing and supporting the innovations we incubate. The team of seven handles talent scouting, recruitment, onboarding, travel, career development, administrative support, and other initiatives to keep those working with us happy, engaged, and productive.',
+        'Our PeopleOps department ensures that our startups have the most valuable asset they need — highly engaged and productive contributors — to focus on developing and supporting the innovations we incubate. The team of seven handles talent scouting, recruiting, onboarding, travel, administrative support, and other initiatives to keep those working with us happy, engaged, and productive.',
     },
     {
       id: '5',
       title: 'Branding and awareness',
       description:
         'Ned Karlovich draws on experience as Creative Director at Nike Inc. and staat to lead the IFT’s creative branding studio, Acid. The studio works with our startups to deploy high-impact campaigns, raising brand awareness among their target markets both within the web3 ecosystem and beyond it. IFT startups enjoy full access to the studio, which comprises marketing, digital communications, design, web development, and events management specialists.',
+      button: {
+        text: 'Discover Acid',
+        url: 'https://acid.info/',
+      },
     },
   ]
 
