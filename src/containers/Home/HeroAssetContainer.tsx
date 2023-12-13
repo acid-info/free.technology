3cde73b261fa5f6a8d206b98d10fd61fa58fe80a
jinhojang6 <jinho5051@gmail.com>
2023-12-13 21:30:27 +0900

    feat: replace hero image

--- src/containers/Home/HeroAssetContainer.tsx
+++ src/containers/Home/HeroAssetContainer.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export const HeroAssetContainer: React.FC = () => {
   return (
     <Container>
-      <Image src="hero-image.png" alt="right-side" />
+      <Image src="right-side.gif" alt="right-side" />
     </Container>
   )
 }
