c847b8cd2a789c55c9ce1e3a080f0a12144e1654
jinhojang6 <jinho5051@gmail.com>
2024-06-04 02:27:11 +0900

    chore: update security page

--- src/pages/security.tsx
+++ src/pages/security.tsx
@@ -8,7 +8,7 @@ We take security seriously at IFT and across the Institute of Free Technology an
 
 Please report any security incidents via <a href="mailto:security@free.technology">security@free.technology</a>. 
 
-Please report any discovered vulnerabilities in our bounty programme at <a href="https://hackenproof.com/status-dot-app" target="_blank">HackenProof</a> to help ensure our protocols and software remain secure.
+Please report any discovered vulnerabilities in our bounty programme at <a href="https://hackenproof.com/ift" target="_blank">HackenProof</a> to help ensure our protocols and software remain secure.
 `
 
 const Page = () => {
